96a03e28bd494deb8769f4cbc940de1d9145eac2
jubindas <jubinrohidas78@gmail.com>
2025-11-20 15:37:34 +0530

    add brances added

--- src/components/AddBrances.tsx
+++ src/components/AddBrances.tsx
@@ -24,6 +24,7 @@ import {
 } from "@/components/ui/select";
 
 export default function AddBrances() {
+  
   const [open, setOpen] = useState(false);
 
   const [district, setDistrict] = useState("");
